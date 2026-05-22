25b723f25768bf3424ccbd6c1b05ae45b30f3883
miromnb-coder <miromnb@icloud.com>
2026-05-22 17:06:13 +0300

    Add centralized Auren haptics

---
+++ src/lib/aurenHaptics.ts
@@ -0,0 +1,52 @@
+import * as Haptics from 'expo-haptics';
+import { Platform } from 'react-native';
+
+async function safeHaptic(run: () => Promise<void>) {
+  if (Platform.OS === 'web') {
+    return;
+  }
+
+  try {
+    await run();
+  } catch (error) {
+    console.log('Auren haptics error:', error);
+  }
+}
+
+export const aurenHaptics = {
+  softTap() {
+    return safeHaptic(() => Haptics.selectionAsync());
+  },
+
+  selection() {
+    return safeHaptic(() => Haptics.selectionAsync());
+  },
+
+  panelOpen() {
+    return safeHaptic(() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light));
+  },
+
+  panelClose() {
+    return safeHaptic(() => Haptics.selectionAsync());
+  },
+
+  sendMessage() {
+    return safeHaptic(() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light));
+  },
+
+  answerStart() {
+    return safeHaptic(() => Haptics.selectionAsync());
+  },
+
+  answerComplete() {
+    return safeHaptic(() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium));
+  },
+
+  success() {
+    return safeHaptic(() => Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success));
+  },
+
+  warning() {
+    return safeHaptic(() => Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning));
+  },
+};
